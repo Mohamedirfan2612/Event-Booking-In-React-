fad996f7256fa12569c793b44207e918c5e64b14
MohamedIrfan <132117843+Mohamedirfan2612@users.noreply.github.com>
2025-07-24 11:38:02 +0530

    Add files via upload

---
+++ Event-Booking/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TicketBookingApp from './Components/TicketBookApp'
+
+const App = () => {
+  return (
+    <div>
+
+      <TicketBookingApp/>
+      
+    </div>
+  )
+}
+
+export default App
